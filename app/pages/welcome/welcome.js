2eb5dd5136a8994d7b5718c95cbeabf23063027b
Anastasiia Timashova <31852525+timashana@users.noreply.github.com>
2018-11-10 15:27:43 -0500

    remove js

--- app/pages/welcome/welcome.js
+++ app/pages/welcome/welcome.js
@@ -1,14 +1,5 @@
 import {Page, NavController, NavParams} from 'ionic-angular';
 
-var canvasSize = [1200, 650],
-      svg        = d3.select("svg"),
-      g       = svg.append("g");
-  g.append('rect')
-    .attr('x',100)
-  .attr('y',100)
-  .attr('width',100)
-  .attr('height',100);
-
 @Page({
     templateUrl: 'build/pages/welcome/welcome.html'
 })
